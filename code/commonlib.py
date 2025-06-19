b8a468d808f3598d4f12a41acb1ebbbd1fad1740
RandomGuyNumber731 <87827918+RandomGuyNumber731@users.noreply.github.com>
2025-06-19 23:26:02 +0300

    major tweaks and fixes

--- code/commonlib.py
+++ code/commonlib.py
@@ -5,7 +5,22 @@
 #
 # all credits go to me and some stack overflow fellars
 
-import requests, os, math, time
+import requests, os, math, time, json
+# make sure u have those ↑
+
+
+
+# PRERUN
+root=os.path.dirname(os.path.abspath(__file__))
+where=os.path.join(root,'__test__')
+wherehp=os.path.join(where, 'hp.json')
+wherecofl=os.path.join(where, 'cofl.json')              # so that i wont need to call api a billion times just to check if i didnt made a typo
+try:
+    os.mkdir(where)
+except FileExistsError:
+    pass
+except Exception as e: raise(e)
+
 
 # MISC
 def echo():
@@ -21,27 +36,53 @@ def check_size():
 
 
 # API
-def callhp():
+def callhp(IsTest=False):
+    IsThere=os.path.isfile(wherehp)
     i='https://api.hypixel.net/skyblock/bazaar'
-    j=requests.get(i)    
-    l=j.json()
+    if not IsTest: j=requests.get(i); l=j.json()
+    else:
+        if IsThere:
+            with open(wherehp) as file: l=json.load(file)
+        else:
+            j=requests.get(i); l=j.json()
+            with open(wherehp,'w') as file: json.dump(l, file, indent=4)
     return l
 
-def callcofl(x=None):
-    if x==None: raise Exception('no value provided')                                                                                        # thanks coflnet for existing and making checking auction alot easier
-    i=f'https://sky.coflnet.com/api/auctions/tag/{x}'
-    j=requests.get(i)
-    l=j.json()
+def callcofl(target=None, IsTest=False):
+    IsThere=os.path.isfile(wherecofl)
+    if target==None: raise Exception('no value provided')                                                                                   # thanks coflnet for existing and making checking auction alot easier
+    i=f'https://sky.coflnet.com/api/auctions/tag/{target}/active/bin'
+    if not IsTest: j=requests.get(i); l=j.json()
+    else:
+        if IsThere:
+            with open(wherecofl) as file: l=json.load(file)
+        else:
+            j=requests.get(i); l=j.json()
+            with open(wherecofl,'w') as file: json.dump(l, file, indent=4)
     return l
 
 
 # QOL/VISUAL
-def short(x=None):
-    i = str(f'{x/1000000000:.2f}')+'B' if abs(x)>=1000000000 else str(f'{x/1000000:.2f}')+'M' if abs(x)>=1000000 else str(f'{x/1000:.2f}')+'k' if abs(x)>=1000 else x
+def short(target=None):
+    i = str(f'{target/1000000000:.2f}')+'B' if abs(target)>=1000000000 else str(f'{target/1000000:.2f}')+'M' if abs(target)>=1000000 else str(f'{target/1000:.2f}')+'k' if abs(target)>=1000 else target
     return i
+
+def long(x=None):
+    try:
+        mul=[1000000000,1000000,1000]
+        for i,j in enumerate(['b','m','k']):
+            if str(x[len(str(x))-1]).lower() == j:
+                x=list(x.replace(',','.'))
+                x.pop(len(x)-1)                         # this was harder than the shortening one
+                x.pop(0)
+                x=float(''.join(x))*mul[i]
+                return x
+        return float(x[1:])
+    except ValueError: return 1
 
 def smol(col, row):
     cl()
+    lastsize=[]
     ohno='window too small!!'
     uneed='u need {} by {}'.format(col, row)
     size = check_size()
@@ -56,10 +97,11 @@ def smol(col, row):
         up=f'{space}^\n{space}|\n{' '*(mid-len(str(size[1]))//2)}{size[1]}'
         down=f'{size[1]}\n{space}|\n{space}V'
         hor=f'<-{size[0]}{' '*(size[0]-4-len(str(size[0])*2))}{size[0]}->'
-        print(f'{'\033[32m' if row<=size[1] else ''}{up}\033[0m\n{'\n'*(math.ceil(size[1]/2)-4)}{' '*ohnomid}{ohno}\n{'\033[32m' if col<=size[0] else ''}{hor}\033[0m\n{' '*uneedmid}{uneed}{'\n'*(size[1]-(math.ceil(size[1]/2))-4)}{' '*(mid-len(str(size[1]))//2)}{'\033[32m' if row<=size[1] else ''}{down}\033[0m', end='', flush=True)    # i have a bad habit of making those...
-        time.sleep(0.05)
-        cl()
-    size = check_size()
+        if lastsize!=size:                                                      # probably fixes too much flickering    
+            cl()
+            print(f'{'\033[32m' if row<=size[1] else ''}{up}\033[0m\n{'\n'*(math.ceil(size[1]/2)-4)}{' '*ohnomid}{ohno}\n{'\033[32m' if col<=size[0] else ''}{hor}\033[0m\n{' '*uneedmid}{uneed}{'\n'*(size[1]-(math.ceil(size[1]/2))-4)}{' '*(mid-len(str(size[1]))//2)}{'\033[32m' if row<=size[1] else ''}{down}\033[0m', end='', flush=True)    # i have a bad habit of making those...
+            time.sleep(0.05); lastsize=size
+    cl()
     print(f'{'\n'*int(size[1]/2)}{' '*int(size[0]/2-5)}well done!!\n')
     time.sleep(0.7)
     cl()
--- code/keygen.py
+++ code/keygen.py
@@ -10,9 +10,12 @@ main='ENCHANTED_{}'
 mainsub='{}'
 sub='BEJEWELED_HANDLE'
 subsub='GLACITE_JEWEL'
+
 data=None
 tp=None
 amnt=1
+hlfs, hlns='',''
+escc='\033[0m'
 
 maina=192
 mainsuba=30720
@@ -25,24 +28,27 @@ def recall():
     cl()
     print('fetching...', flush=True, end='')
     global data
-    data = callhp()
+    data = callhp(True)
 
 def put():
     cl()
     global data, tp
     i=input('MENU\n| 1 - tungsten\n| 2 - umber\n-| r - recall api\n-| q - exit\n\ninput: ')
-    match i[0]:
-        case '1': tp='TUNGSTEN'; calc(tp)
-        case '2': tp='UMBER'; calc(tp)
-        case 'r': recall() ; put()
-        case 'q': cl() ; quit()
-        case   _: put()
+    try:
+        match i[0]:
+            case '1': tp='TUNGSTEN'; calc(tp)                                                   # so far the best design
+            case '2': tp='UMBER'; calc(tp)
+            case 'r': recall() ; put()
+            case 'q': cl() ; quit()
+            case   _: put()
+    except IndexError: put()
 
 
 def calc(target=None):
+    smol(93,30)
     cl()
     if target==None: raise Exception('no value provided')
-    global name,main,mainsub,sub,subsub,mul,amnt
+    global name,main,mainsub,sub,subsub,mul,amnt,hlns,hlfs
     name_=name.format(target)
     main_=main.format(target)
     mainsub_=target
@@ -58,7 +64,7 @@ def calc(target=None):
         if i!=0: need = amnt*mul[i] ; order = need//71680 ; part = need-order*71680
         else: need=amnt*mul[i]; order=part=''
         blk='\033[33m' if not isinstance(order, str) and 20<order<25 else '\033[31m' if not isinstance(order, str) and order>=25 else ''
-        print(f'{blk} {j:<22}{pb:<14}{ps:<14}{need:<14}{order:<10}{part:<14}\033[0m')
+        print(f'{blk} {j:<22}{pb:<14}{ps:<14}{need:<14}{order:<10}{part}\033[0m')
     print('\n\033[90m{:<22}{:<14}{:<14}\033[0m'.format('','cost insta:','cost order:'))
     for i,j in enumerate(items):
             ci = short(item_buy[j]*amnt*mul[i])
@@ -71,19 +77,19 @@ def calc(target=None):
         if i!=0:
             costo[j]=item_sell[j]*mul[i]
             costi[j]=item_buy[j]*mul[i]
-    print('\n\n\033[90m{:<18}{:<20}{:<20}{:<20}{:<20}\033[0m'.format('','full sub profit:','sub handle only:',f'sub {mainsub_.lower()} only:','no sub:'))
+    print('\n\n\033[90m{:<18}{:<20}{:<20}{:<20}{}\033[0m'.format('','full sub profit:','sub handle only:',f'sub {mainsub_.lower()} only:','no sub:'))
     for i,j in enumerate(ioocnfmv):
         if i==0:
-            fsp=short(costo[mainsub_]*amnt+costo[subsub]*amnt)
-            sho=short(costo[main_]*amnt+costo[subsub]*amnt)
-            smo=short(costo[mainsub_]*amnt+costo[sub]*amnt)
-            ns =short(costo[main_]*amnt+costo[sub]*amnt)
+            fsp=hlfs+short(costo[mainsub_]*amnt+costo[subsub]*amnt)+escc
+            sho=' '*(len(hlfs)+len(escc))+short(costo[main_]*amnt+costo[subsub]*amnt)                       # i did that for no apparent reason and im not sure why would it be useful
+            smo=' '*(len(hlfs)+len(escc))+short(costo[mainsub_]*amnt+costo[sub]*amnt)                       # love me some shitty workarounds
+            ns =' '*(len(hlfs)+len(escc))+hlns+short(costo[main_]*amnt+costo[sub]*amnt)+escc
         else:
             fsp=short(ioocnfmva[i][name_]*amnt-costo[mainsub_]*amnt-costo[subsub]*amnt)
             sho=short(ioocnfmva[i][name_]*amnt-costo[main_]*amnt-costo[subsub]*amnt)
             smo=short(ioocnfmva[i][name_]*amnt-costo[mainsub_]*amnt-costo[sub]*amnt)
             ns =short(ioocnfmva[i][name_]*amnt-costo[main_]*amnt-costo[sub]*amnt)
-        print(' {:<18}{:<20}{:<20}{:<20}{:<20}'.format(j,fsp,sho,smo,ns))
+        print(' {:<18}{:<20}{:<20}{:<20}{}'.format(j,fsp,sho,smo,ns))
     print('\033[90mbying stuff instantly:\033[0m')
     for i,j in enumerate(ioocnfmv):
         if i==0:
@@ -96,20 +102,24 @@ def calc(target=None):
             sho=short(ioocnfmva[i][name_]*amnt-costi[main_]*amnt-costi[subsub]*amnt)
             smo=short(ioocnfmva[i][name_]*amnt-costi[mainsub_]*amnt-costi[sub]*amnt)
             ns =short(ioocnfmva[i][name_]*amnt-costi[main_]*amnt-costi[sub]*amnt)
-        print(' {:<18}{:<20}{:<20}{:<20}{:<20}'.format(j,fsp,sho,smo,ns))
+        print(' {:<18}{:<20}{:<20}{:<20}{}'.format(j,fsp,sho,smo,ns))
 
 
-    l=input('\n\ntype in any number for amount (max 20000) | q - go back: ')
+    l=input('\n\n| type in any number for amount (max 20000)\n| use "!" (no sub) or "~" (full sub) prefix for max amount for ur budget\n| q - go back: ')
     try:
+        hlns, hlfs='',''
         l=math.floor(float(l))
+        if l<=0: l=1
         if l>20000: l=20000
     except: l=l
-    match l:
-        case 'q': amnt=1; put()
-        case   _:
-            if isinstance(l, (int,float)): amnt=l; calc(tp)
-            else: calc(tp)
+    try:
+        match str(l)[0]:
+            case 'q': amnt=1; put()
+            case '!': hlns='\033[33m'; amnt=int(long(l)//(costo[main_]+costo[sub])); amnt+=1 if amnt==0 else 0; amnt=20000 if amnt>20000 else amnt; calc(tp)
+            case '~': hlfs='\033[33m'; amnt=int(long(l)//(costo[mainsub_]+costo[subsub])); amnt+=1 if amnt==0 else 0; amnt=20000 if amnt>20000 else amnt; calc(tp)
+            case   _: amnt=int(l)if isinstance(l, int) else 1; calc(tp)
+    except IndexError: amnt=1; calc(tp)
 
-smol(101,29)
+
 recall()
 put()
